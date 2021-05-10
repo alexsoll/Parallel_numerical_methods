c5e42e619154937945e3afa4756fa2bcc7b6e02d
Alexsey Soluyanov <al.soluyanov@rambler.ru>
2021-05-10 22:52:48 +0300

    dirihlet: upd

--- Dirihlet/main.cpp
+++ Dirihlet/main.cpp
@@ -32,7 +32,7 @@ void heat_dirichlet_sor(heat_task task, double* v) {
 
     double D = 2 * (h_ + k_);
 
-    double eps = 1e-5;
+    double eps = 1e-15;
 
     vector<vector<double>> nu(task.n + 1, vector<double>(task.m + 1, 0.0));
 
@@ -48,7 +48,6 @@ void heat_dirichlet_sor(heat_task task, double* v) {
         nu[task.n][i] = task.right_condition(i * k);
     }
 
-
     // The function of external impact
     vector<vector<double>> f(task.n + 1, vector<double>(task.m + 1, 0.0));
 #pragma omp parallel for
@@ -58,35 +57,37 @@ void heat_dirichlet_sor(heat_task task, double* v) {
         }
     }
 
-    double err;
+    int j;
     double tmp;
-    int spendIter = 0;
-    double dmax;
+    double err;
+    double prev;
     do {
-        dmax = 0;
         err = 0.;
-        spendIter++;
-        int j;
         for (int k = 0; k < task.n + task.m - 3; k++) {
+#pragma omp parallel for private(j, tmp, prev)
             for (int i = min(1 + k, task.n - 1); i >= max(1, k - task.m + 3); i--) {
                 j = task.m - (k - i + 2);
-                tmp = nu[i][j];
 
-                nu[i][j] = omega * h_ * nu[i - 1][j] + omega * k_ * nu[i][j - 1] + omega * h_ * nu[i + 1][j] + omega * k_ * nu[i][j + 1] + \
-                    (1 - omega) * D * nu[i][j] + omega * f[i][j];
-                nu[i][j] /= D;
+                prev = nu[i][j];
+                tmp = - D * prev + h_ * (nu[i - 1][j] + nu[i + 1][j]) + k_ * (nu[i][j - 1] + nu[i][j + 1]);
 
+                nu[i][j] = prev + omega * (tmp + f[i][j]) / D;
 
-                err = fabs(tmp - nu[i][j]);
-                if (dmax < err) dmax = err;
+                tmp = fabs(nu[i][j] - prev);
+
+#pragma omp critical
+                {
+                    if (tmp > err) {
+                        err = tmp;
+                    }
+                }
             }
         }
-    } while (dmax > eps);
+    } while (tmp > eps);
 
-    std::cout << "spend iter : " << spendIter << std::endl;
+#pragma omp parallel for
     for (int i = 0; i <= task.n; i++)
     {
-#pragma omp parallel for
         for (int j = 0; j <= task.m; j++)
         {
             v[i * (task.m + 1) + j] = nu[i][j];
@@ -105,10 +106,11 @@ void PrintArray(int size, double* a) {
 int main(int argc, char* argv[]) {
     heat_task t;
     double* v = new double[(t.n + 1) * (t.m + 1)];
-    std::cout << "Size of vector : " << (t.n + 1) * (t.m + 1) << std::endl;
+
     heat_dirichlet_sor(t, v);
-
+    
     PrintArray((t.n + 1) * (t.m + 1), v);
-
+    
+    delete[] v;
     return 0;
 }
