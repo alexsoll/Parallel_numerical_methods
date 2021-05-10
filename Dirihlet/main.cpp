606c618b8bd752eb41c4a75503f4681c1376318e
Alexsey Soluyanov <al.soluyanov@rambler.ru>
2021-05-10 17:51:11 +0300

    dirihlet: add wave algorithm

--- Dirihlet/main.cpp
+++ Dirihlet/main.cpp
@@ -37,11 +37,13 @@ void heat_dirichlet_sor(heat_task task, double* v) {
     vector<vector<double>> nu(task.n + 1, vector<double>(task.m + 1, 0.0));
 
     // Boundary
+#pragma omp parallel for
     for (int i = 0; i <= task.n; i++) {
         nu[i][0] = task.bottom_condition(i * h);
         nu[i][task.m] = task.top_condition(i * h);
     }
-    for (int i = 0; i <= task.n; i++) {
+#pragma omp parallel for
+    for (int i = 0; i <= task.m; i++) {
         nu[0][i] = task.left_condition(i * k);
         nu[task.n][i] = task.right_condition(i * k);
     }
@@ -49,30 +51,42 @@ void heat_dirichlet_sor(heat_task task, double* v) {
 
     // The function of external impact
     vector<vector<double>> f(task.n + 1, vector<double>(task.m + 1, 0.0));
+#pragma omp parallel for
     for (int i = 0; i <= task.n; i++) {
         for (int j = 0; j <= task.m; j++) {
-            f[i][j] = task.f(i * h, j * k);
+            f[i][j] = - task.f(i * h, j * k);
         }
     }
 
-    int maxIter = 50;
-    //double err;
+    double err;
+    double tmp;
+    int spendIter = 0;
+    double dmax;
+    do {
+        dmax = 0;
+        err = 0.;
+        spendIter++;
+        int j;
+        for (int k = 0; k < task.n + task.m - 3; k++) {
+            for (int i = min(1 + k, task.n - 1); i >= max(1, k - task.m + 3); i--) {
+                j = task.m - (k - i + 2);
+                tmp = nu[i][j];
 
-    for (int iter = 0; iter < maxIter; iter++) {
-        //err = 0.;
-        for (int i = 1; i < task.n; i++) {
-            for (int j = 1; j < task.m; j++) {                
                 nu[i][j] = omega * h_ * nu[i - 1][j] + omega * k_ * nu[i][j - 1] + omega * h_ * nu[i + 1][j] + omega * k_ * nu[i][j + 1] + \
                     (1 - omega) * D * nu[i][j] + omega * f[i][j];
                 nu[i][j] /= D;
-                std::cout << "nu[" << i << "][" << j << "] = " << nu[i][j] << std::endl;
+
+
+                err = fabs(tmp - nu[i][j]);
+                if (dmax < err) dmax = err;
             }
         }
-    }
+    } while (dmax > eps);
 
-
+    std::cout << "spend iter : " << spendIter << std::endl;
     for (int i = 0; i <= task.n; i++)
     {
+#pragma omp parallel for
         for (int j = 0; j <= task.m; j++)
         {
             v[i * (task.m + 1) + j] = nu[i][j];
